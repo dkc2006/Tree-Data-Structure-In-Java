9d07607d0c253554bbf4d92950d42ba6cd7e7b42
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-04-01 22:01:57 +0530

    first commit

--- src/main/java/tree/binarySearchTree/BinarySearchTree.java
+++ src/main/java/tree/binarySearchTree/BinarySearchTree.java
@@ -1,4 +1,29 @@
-package tree.binarySearchTree;
+package tree;
 
 public class BinarySearchTree {
+    int data;
+    BinarySearchTree leftNode;
+    BinarySearchTree rightNode;
+
+    public BinarySearchTree(int data) {
+        this.data = data;
+    }
+
+}
+
+class BinaryTree {
+    BinarySearchTree rootNode;
+
+    void insertNode(int data) {
+        BinarySearchTree node = new BinarySearchTree(data);
+        if (rootNode == null) {
+            rootNode = node;
+        }
+        else{
+            BinarySearchTree currentNode = rootNode;
+            BinarySearchTree parentNode;
+        }
+
+
+    }
 }
