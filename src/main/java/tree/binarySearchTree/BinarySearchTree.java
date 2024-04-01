443a83ee9b4ff9484188ec860145f9892f3fd3f6
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-04-01 23:41:40 +0530

    first commit

--- src/main/java/tree/binarySearchTree/BinarySearchTree.java
+++ src/main/java/tree/binarySearchTree/BinarySearchTree.java
@@ -1,4 +1,4 @@
-package tree;
+package tree.binarySearchTree;
 
 public class BinarySearchTree {
     int data;
@@ -18,12 +18,54 @@ class BinaryTree {
         BinarySearchTree node = new BinarySearchTree(data);
         if (rootNode == null) {
             rootNode = node;
-        }
-        else{
+        } else {
             BinarySearchTree currentNode = rootNode;
             BinarySearchTree parentNode;
+            while (true) {
+                parentNode = currentNode;
+                if (data < currentNode.data) {
+                    currentNode = currentNode.leftNode;
+                    if (currentNode == null) {
+                        parentNode.leftNode = node;
+                        return;
+                    }
+                } else {
+                    currentNode = currentNode.rightNode;
+                    if (currentNode == null) {
+                        parentNode.rightNode = node;
+                        return;
+                    }
+                }
+            }
         }
+    }
+}
 
-
+class Main {
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.rootNode = new BinarySearchTree(53);
+        BinarySearchTree thirty = new BinarySearchTree(30);
+        BinarySearchTree seventyTwo = new BinarySearchTree(72);
+        BinarySearchTree fourteen = new BinarySearchTree(14);
+        BinarySearchTree thirtyNine = new BinarySearchTree(39);
+        BinarySearchTree sixtyOne = new BinarySearchTree(61);
+        BinarySearchTree eightyFour = new BinarySearchTree(84);
+        BinarySearchTree seventyNine = new BinarySearchTree(79);
+        BinarySearchTree nine = new BinarySearchTree(9);
+        BinarySearchTree TwentyThree = new BinarySearchTree(23);
+        BinarySearchTree thirtyFour = new BinarySearchTree(34);
+        BinarySearchTree fourtySeven = new BinarySearchTree(47);
+        binaryTree.rootNode.leftNode = thirty;
+        binaryTree.rootNode.rightNode = seventyTwo;
+        thirty.leftNode = fourteen;
+        thirty.rightNode = thirtyNine;
+        seventyTwo.leftNode = sixtyOne;
+        seventyTwo.rightNode = eightyFour;
+        fourteen.leftNode = nine;
+        fourteen.rightNode = TwentyThree;
+        thirtyNine.leftNode = thirtyFour;
+        thirtyNine.rightNode = fourtySeven;
+        eightyFour.leftNode = seventyNine;
     }
 }
